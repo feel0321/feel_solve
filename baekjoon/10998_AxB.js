2b02d57783054613275287fec70a4d2e1ee97983
feel0321 <dishate@naver.com>
2022-01-05 00:02:02 +0900

    백준_10998_AxB.js
    백준_10998_AxB.js

---
+++ baekjoon/10998_AxB.js
@@ -0,0 +1,14 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  const [num1, num2] = line.split(" ");
+  console.log(num1 * num2);
+  rl.close();
+}).on("close", function () {
+  process.exit();
+});
