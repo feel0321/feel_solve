cbd533619bdf579389e84e9173639f536a6c4a80
feel0321 <dishate@naver.com>
2022-01-08 23:59:16 +0900

    백준_1330_두 수 비교하기.js
    백준_1330_두 수 비교하기.js

---
+++ baekjoon/1330_두 수 비교하기.js
@@ -0,0 +1,20 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  const [num1, num2] = line.split(" ");
+  console.log(solution(Number(num1), Number(num2)));
+  rl.close();
+}).on("close", function () {
+  process.exit();
+});
+
+function solution(a, b) {
+  if (a > b) return ">";
+  if (a < b) return "<";
+  return "==";
+}
