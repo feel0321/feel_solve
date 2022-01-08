de4bc8c92640cd7bb27fb774258f8f22045bc381
feel0321 <dishate@naver.com>
2022-01-08 23:59:47 +0900

    백준_9498_시험성적.js
    백준_9498_시험성적.js

---
+++ baekjoon/9498_시험 성적.js
@@ -0,0 +1,21 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  console.log(solution(Number(line)));
+  rl.close();
+}).on("close", function () {
+  process.exit();
+});
+
+function solution(score) {
+  if (score >= 90) return "A";
+  if (score >= 80) return "B";
+  if (score >= 70) return "C";
+  if (score >= 60) return "D";
+  return "F";
+}
