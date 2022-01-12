cebc9942e8b680dc8b0907d93a4c559cd8a6a746
feel0321 <dishate@naver.com>
2022-01-12 23:47:35 +0900

    백준_11022_A+B-8.js
    백준_11022_A+B-8.js

---
+++ baekjoon/11022_A+B-8.js
@@ -0,0 +1,17 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let caseCount = 1;
+rl.on("line", function (line) {
+  const inputLine = line.split(" ");
+  if (inputLine.length > 1) {
+    const [num1, num2] = inputLine.map((element) => Number(element));
+    console.log(`Case #${caseCount++}: ${num1} + ${num2} = ${num1 + num2}`);
+  }
+}).on("close", function () {
+  process.exit();
+});
