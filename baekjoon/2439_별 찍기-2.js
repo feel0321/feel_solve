e352760ad04d647e6765444d0997e7660786cfe8
feel0321 <dishate@naver.com>
2022-01-12 23:53:46 +0900

    백준_2439_별 찍기-2.js
    백준_2439_별 찍기-2.js

---
+++ baekjoon/2439_별 찍기-2.js
@@ -0,0 +1,18 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  solution(Number(line));
+  rl.close();
+}).on("close", function () {
+  process.exit();
+});
+
+function solution(number) {
+  for (let i = 1; i <= number; i++)
+    console.log(" ".repeat(number - i) + "*".repeat(i));
+}
