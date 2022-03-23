20e677b4b92b4f7cbf329cac64aa20d0f9e8026a
feel0321 <dishate@naver.com>
2022-03-23 18:06:54 +0900

    백준_15829_Hashing.js 100점 코드
    백준_15829_Hashing.js 100점 코드

--- baekjoon/15829_Hashing.js
+++ baekjoon/15829_Hashing.js
@@ -5,6 +5,22 @@ let [len, string] = fs.readFileSync(filePath).toString().trim().split("\n");
 
 console.log(solution(string));
 
+function solution(str) {
+  let r = 1;
+  const R = 31;
+  const m = 1234567891;
+  let answer = 0;
+  for (let i = 0; i < len; i++) {
+    answer += (str.charCodeAt(i) - 96) * r;
+    answer %= m;
+    r *= R;
+    r %= m;
+    // r이 지수승이라 엄청 커지니까 루프마다 %m
+  }
+  return answer;
+}
+
+/* 50점 코드
 function solution(string) {
   const r = 31;
   const m = 1234567891;
@@ -19,3 +35,4 @@ function solution(string) {
   });
   return answer % m;
 }
+*/
