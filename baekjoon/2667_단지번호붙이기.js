c2870a41501c90a345906494d50cd17bd068a917
feel0321 <dishate@naver.com>
2023-01-30 23:45:58 +0900

    feat: 백준_2667_단지번호붙이기.js

---
+++ baekjoon/2667_단지번호붙이기.js
@@ -0,0 +1,52 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "../input.txt";
+
+let [n, ...ground] = fs.readFileSync(filePath).toString().trim().split("\n");
+/*
+    0,0부터 돌면서 0이 아닌 값이면 주변 탐색
+*/
+const visited = Array.from({ length: n }, () => Array(n).fill(false));
+const ZERO = "0";
+
+console.log(solution(ground, visited));
+
+function solution(ground, visited) {
+  const n = ground.length;
+  const answer = [];
+  let count = 0;
+
+  for (let i = 0; i < n; i++) {
+    for (let i2 = 0; i2 < n; i2++) {
+      if (ground[i][i2] !== ZERO && !visited[i][i2]) {
+        dfs(i, i2);
+        answer.push(count);
+        count = 0;
+      }
+    }
+  }
+
+  function dfs(y, x) {
+    visited[y][x] = true;
+    count += 1;
+    const dy = [0, 1, 0, -1];
+    const dx = [1, 0, -1, 0];
+
+    for (let i = 0; i < 4; i++) {
+      const nextY = y + dy[i];
+      const nextX = x + dx[i];
+      if (
+        nextY >= 0 &&
+        nextY < n &&
+        nextX >= 0 &&
+        nextX < n &&
+        ground[nextY][nextX] !== ZERO &&
+        !visited[nextY][nextX]
+      ) {
+        dfs(nextY, nextX);
+      }
+    }
+  }
+
+  answer.sort((a, b) => a - b);
+  return `${answer.length}\n${answer.join("\n")}`;
+}
