d8cef561fd0522bda253c8d70619e976b6dc3bb9
feel0321 <dishate@naver.com>
2023-02-04 22:55:07 +0900

    feat: 백준_2583_영역 구하기.js BFS 추가

--- baekjoon/2583_영역 구하기.js
+++ baekjoon/2583_영역 구하기.js
@@ -8,8 +8,12 @@ let [firstLine, ...input] = fs
   .split("\n");
 const [yLength, xLength] = firstLine.split(" ").map(Number);
 
-const answer = solution(yLength, xLength, input);
-console.log(answer);
+const dfsAnswer = solution(yLength, xLength, input);
+console.log("dfs");
+console.log(dfsAnswer);
+const bfsAnswer = solution2(yLength, xLength, input);
+console.log("bfs");
+console.log(bfsAnswer);
 
 function solution(yLength, xLength, input) {
   const [ground, visited] = setGround(input);
@@ -46,6 +50,54 @@ function solution(yLength, xLength, input) {
         dfs(nextY, nextX);
       }
     }
+  }
+
+  return `${answer.length}\n${answer.sort((a, b) => a - b).join(" ")}`;
+}
+
+function solution2(yLength, xLength, input) {
+  const [ground, visited] = setGround(input);
+  const answer = [];
+
+  for (let y = 0; y < yLength; y++) {
+    for (let x = 0; x < xLength; x++) {
+      if (!visited[y][x] && !ground[y][x]) {
+        const count = bfs(y, x);
+        answer.push(count);
+      }
+    }
+  }
+
+  function bfs(y, x) {
+    const queue = [];
+    queue.push([y, x]);
+    visited[y][x] = true;
+    let count = 1;
+    const dy = [1, 0, -1, 0];
+    const dx = [0, 1, 0, -1];
+
+    while (queue.length) {
+      const [currentY, currentX] = queue.shift();
+      for (let i = 0; i < 4; i++) {
+        const nextY = dy[i] + currentY;
+        const nextX = dx[i] + currentX;
+
+        if (
+          nextY >= 0 &&
+          nextY < yLength &&
+          nextX >= 0 &&
+          nextX < xLength &&
+          !ground[nextY][nextX] &&
+          !visited[nextY][nextX]
+        ) {
+          queue.push([nextY, nextX]);
+          visited[nextY][nextX] = true;
+          count += 1;
+        }
+      }
+    }
+
+    return count;
   }
 
   return `${answer.length}\n${answer.sort((a, b) => a - b).join(" ")}`;
