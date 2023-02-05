92cf7369658aefc6496b19298f90f574ec3553f7
feel0321 <dishate@naver.com>
2023-02-05 23:36:16 +0900

    feat: 백준_2644_촌수계산.js BFS 추가. refactor: 공통 준비코드 분리

--- baekjoon/2644_촌수계산.js
+++ baekjoon/2644_촌수계산.js
@@ -9,11 +9,12 @@ let [n, first, m, ...input] = fs
 n = Number(n);
 const [start, end] = first.split(" ").map(Number);
 m = Number(m);
-
-const answer = solution(input, start, end, n);
+const answer = solutionDFS(input, start, end, n);
 console.log(answer);
+const answerBFS = solutionBFS(input, start, end, n);
+console.log(answerBFS);
 
-function solution(input, start, end, n) {
+function solutionDFS(input, start, end, n) {
   const ground = Array.from({ length: n + 1 }, () => Array(n + 1).fill(false));
   const visited = Array(n + 1).fill(false);
   let answer = -1;
@@ -40,4 +41,45 @@ function solution(input, start, end, n) {
       }
     }
   }
+}
+
+function solutionBFS(input, start, end, n) {
+  const [ground, visited] = setGround(n, input);
+
+  return bfs(start, end);
+
+  function bfs(start, end) {
+    const queue = [];
+    queue.push([start, 0]);
+    visited[start] = true;
+
+    while (queue.length) {
+      const [current, count] = queue.shift();
+      if (current === end) {
+        return count;
+      }
+
+      for (let i = 1; i <= n; i++) {
+        if (ground[current][i] && !visited[i]) {
+          queue.push([i, count + 1]);
+          visited[i] = true;
+        }
+      }
+    }
+
+    return -1;
+  }
+}
+
+function setGround(n, input) {
+  const ground = Array.from({ length: n + 1 }, () => Array(n + 1).fill(false));
+  const visited = Array(n + 1).fill(false);
+
+  input.forEach((line) => {
+    const [man1, man2] = line.split(" ").map(Number);
+    ground[man1][man2] = true;
+    ground[man2][man1] = true;
+  });
+
+  return [ground, visited];
 }
